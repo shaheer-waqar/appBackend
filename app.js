81c6be8421ecefc88035554434aecc38bc3de9e1
shaheer-waqar <shaheerwaqar02@gmail.com>
2024-11-09 01:11:35 +0500

    error fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.use);
+app.use(cors());
 
 dbConnection()
 const PORT = process.env.PORT || 3000;
